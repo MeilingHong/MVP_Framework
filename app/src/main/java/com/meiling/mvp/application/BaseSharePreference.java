3bbd4b0128ecbf3385fa1a265d13fc787c6ad6d6
MeilingHong <marisareimu@126.com>
2017-07-12 11:08:35 +0800

    Share Prefencens

--- app/src/main/java/com/meiling/mvp/application/BaseSharePreference.java
+++ app/src/main/java/com/meiling/mvp/application/BaseSharePreference.java
@@ -1,5 +1,8 @@
 package com.meiling.mvp.application;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * TODO to store little data with key
  * TODO 存储少量数据的键值对工具
@@ -7,4 +10,79 @@ package com.meiling.mvp.application;
  */
 
 public class BaseSharePreference {
+    private static BaseSharePreference instances;
+    private BaseSharePreference(){
+
+    }
+
+    public BaseSharePreference getInstances(){
+        if(instances==null){
+            instances = new BaseSharePreference();
+        }
+        return  instances;
+    }
+
+    public void setUserId(int userId) {
+        SharedPreferences.Editor editor = BaseApplication.getInstances().getSharedPreferences(ISharePreferenceKey.SP_FILE_NAME, Context.MODE_PRIVATE).edit();
+        editor.putInt(ISharePreferenceKey.KEY_USERID, userId);
+        editor.commit();
+    }
+
+    public void setInt(int common_int) {
+        SharedPreferences.Editor editor = BaseApplication.getInstances().getSharedPreferences(ISharePreferenceKey.SP_FILE_NAME, Context.MODE_PRIVATE).edit();
+        editor.putInt(ISharePreferenceKey.KEY_COMMON_INT, common_int);
+        editor.commit();
+    }
+
+    public void setString(String common_String) {
+        SharedPreferences.Editor editor = BaseApplication.getInstances().getSharedPreferences(ISharePreferenceKey.SP_FILE_NAME, Context.MODE_PRIVATE).edit();
+        editor.putString(ISharePreferenceKey.KEY_COMMON_STRING, common_String);
+        editor.commit();
+    }
+
+    //***********************************
+
+    public void setIntValue(String key,int value) {
+        SharedPreferences.Editor editor = BaseApplication.getInstances().getSharedPreferences(ISharePreferenceKey.SP_FILE_NAME, Context.MODE_PRIVATE).edit();
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+    public void setStringValue(String key,String value) {
+        SharedPreferences.Editor editor = BaseApplication.getInstances().getSharedPreferences(ISharePreferenceKey.SP_FILE_NAME, Context.MODE_PRIVATE).edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public int getIntValue(String key){
+        return BaseApplication.getInstances().getSharedPreferences(ISharePreferenceKey.SP_FILE_NAME,Context.MODE_PRIVATE).getInt(key,-1);
+    }
+
+    public String getStringValue(String key){
+        return BaseApplication.getInstances().getSharedPreferences(ISharePreferenceKey.SP_FILE_NAME,Context.MODE_PRIVATE).getString(key,"");
+    }
+
+    //***********************************
+
+    public void setIntValue(String filename,String key,int value) {
+        SharedPreferences.Editor editor = BaseApplication.getInstances().getSharedPreferences(filename, Context.MODE_PRIVATE).edit();
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+    public void setStringValue(String filename,String key,String value) {
+        SharedPreferences.Editor editor = BaseApplication.getInstances().getSharedPreferences(filename, Context.MODE_PRIVATE).edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public int getIntValue(String filename,String key){
+        return BaseApplication.getInstances().getSharedPreferences(filename,Context.MODE_PRIVATE).getInt(key,-1);
+    }
+
+    public String getStringValue(String filename,String key){
+        return BaseApplication.getInstances().getSharedPreferences(filename,Context.MODE_PRIVATE).getString(key,"");
+    }
+
+
 }
---
+++ app/src/main/java/com/meiling/mvp/application/ISharePreferenceKey.java
@@ -0,0 +1,17 @@
+package com.meiling.mvp.application;
+
+/**
+ * Created by Administrator on 2017/7/12.
+ */
+
+public interface ISharePreferenceKey {
+
+    String SP_FILE_NAME = "CommonStore";
+
+    //TODO Key******************************************
+    String KEY_USERID = "userid";
+
+    String KEY_COMMON_INT = "common_int";
+    String KEY_COMMON_STRING = "common_string";
+
+}
